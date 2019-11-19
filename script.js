9290db0ba92b6b832cf202b5fba4715d54acc415
Doug Arnold <d.m.arnold@ljmu.ac.uk>
2019-11-19 18:28:41 +0000

    Added test of changing field types based on dropdown

--- script.js
+++ script.js
@@ -14,3 +14,27 @@ $(function(){
 $(function(){
  $("#sparm").chained("#spinstrum");
 });
+
+
+$(function()
+{
+    $('#plotMode').change(function(){
+      if ($(this).val() == 'snrMode') {
+         console.log('Mode: SNR');
+      }
+      if ($(this).val() == 'seeMode') {
+         console.log('Mode: Seeing');
+         $('#seeing').replaceWith('<input type="text" data-slider-id="ex1Slider" name="seeing" id="seeing" value="2,3,4,5,6">');
+         $('#sky').replaceWith('<select data-slider-id="ex1Slider" type="text" name="sky" id="sky"></select>');
+      }
+      if ($(this).val() == 'skyMode') {
+         console.log('Mode: Sky');
+         $('#seeing').replaceWith('<select type="text" name="seeing" id="seeing"></select>');
+         $('#sky').replaceWith('<input type="text" name="sky" id="sky" value="2,3,4,5,6">');
+      }
+
+
+    });
+
+}
+);
